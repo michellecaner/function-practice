eb44cc1d3bb9ae69f49472cd5fa2f41e65577194
michellecaner <michellecaner@gmail.com>
2021-11-09 21:53:23 -0600

    mostly completed function practice

---
+++ scripts/fp.js
@@ -0,0 +1,56 @@
+/* 
+  For this practice assignment, make a new JavaScript file that you can use to write code that meets the below requirements.
+*/
+
+// Create an array of your favorite movies
+let faveMovies = ["Auntie Mame", "Splash", "Selena", "Clueless"]
+
+// Create a function to add a movie to the array
+// const addMovie = (newMovie) => {
+//   console.log("Movie to add", newMovie)
+//   faveMovies.push(newMovie)
+// }
+
+// Invoke the above function to add a movie to the array
+// addMovie("Wet Hot American Summer")
+
+// console.log(faveMovies)
+
+// Create a function that loops through your movie array.  Add a conditional statement to differentiates one or more movies from the array.
+// const classicMoviePicker = () => {
+//   for (let i=0; i < faveMovies.length; i++) {
+//     if (faveMovies[i] === "Auntie Mame") {
+//       console.log("This is a classic movie.")
+//     } else {
+//       console.log("This is a modern movie.")
+//     }
+//   }
+// }
+
+// classicMoviePicker()
+
+
+// Create a function that concatinates three stings and returns that value
+// const stringOne = "Happy "
+// const stringTwo = "Birthday "
+// const stringThree = "to you!"
+
+// const sentenceMaker = () => {
+//   const sentence = stringOne + stringTwo + stringThree
+//   return sentence
+//   //console.log(sentence) <-- this works but my return statement does not??
+// }
+
+// // Invoke the function and console log the output
+// sentenceMaker()
+
+// Create a function that console logs the multiplicative of 2 numbers
+const mathTime = (num1, num2) => {
+console.log(num1 * num2)
+}
+
+// Invoke the function
+mathTime(4,4)
+
+// Bonus DataType Practice:
+// create a ternary and console log it.
